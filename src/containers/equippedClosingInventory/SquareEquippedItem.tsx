9ba78ce4077f2dbd2fe09e4b3039a9d521344fc4
lrazerz <lrazerz9ine@gmail.com>
2020-09-22 16:26:04 +0300

    fixed tools equipped item drop

--- src/containers/equippedClosingInventory/SquareEquippedItem.tsx
+++ src/containers/equippedClosingInventory/SquareEquippedItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDrag} from 'react-dnd';
 import {useDispatch} from "react-redux";
 import {addDraggedItem, draggedItemRelease} from "../../redux/actions/draggedItem";
@@ -32,9 +32,38 @@ const SquareEquippedItem = ({item}: { item: any }) => {
     }
   }
 
+  {/*useEffect(() => {*/}
+  //   function imageToDataUri(img, width, height) {
+  //
+  //     // create an off-screen canvas
+  {/*    let canvas = document.createElement('canvas'),*/}
+  {/*      ctx = canvas.getContext('2d');*/}
+
+  //     // set its dimension to target size
+  //     canvas.width = width;
+  //     canvas.height = height;
+  //
+  //     // draw source image into the off-screen canvas:
+  //     ctx.drawImage(img, 0, 0, width, height);
+  //
+  //     // encode image to data-uri with base64 version of compressed image
+  //     return canvas.toDataURL();
+  //   }
+  //
+  //   const resizeImage = () => {
+  //     setResultDataUri(imageToDataUri(newImage, imageWidth, imageHeight));
+  //   }
+  //   const newImage = new Image();
+  //   newImage.onload = resizeImage;
+  //   newImage.src = item.imageUrl;
+  //
+  // }, [item, imageWidth, imageHeight]);
+
   return (
     <EquippedItem forwardedRef={drag}>
-      {item.imageUrl && <img src={item.imageUrl} className={classes.Image} style={imageStyles}/>}
+      {item.imageUrl && <img src={item.imageUrl} className={classes.Image} style={imageStyles}
+
+      />}
       {item.currentCount > 1
       &&
       (<div className={classes.CurrentCount}>
